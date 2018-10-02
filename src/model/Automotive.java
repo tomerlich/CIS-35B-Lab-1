95c21e3cf72ebc07d5b5eab2c4208180a73175ef
tomerlich <tomer.erlich2@gmail.com>
2018-10-01 18:04:52 -0700

    commit

--- src/model/Automotive.java
+++ src/model/Automotive.java
@@ -1,12 +1,27 @@
 package model;
 
 public class Automotive {
+	String name;
+	OptionSet opset[];
+	
+	Automotive(int size, String name){
+		opset = new OptionSet[size];
+		this.name = name;
+	}
+	
 	class OptionSet{
-
-		class Option{
+		Option opt[];
+		String name;
+		
+		OptionSet(String setName, int setSize){
+			opt = new Option[setSize];
+			name = setName;
 			
 		}
 		
+		class Option{
+			String name;
+			float price;
+		}	
 	}
-
 }
