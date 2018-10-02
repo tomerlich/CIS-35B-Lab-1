e5e4406c8655d19fb8a2e8bc613f292a37f76711
tomerlich <tomer.erlich2@gmail.com>
2018-10-01 18:28:11 -0700

    commit adding constructors

--- src/model/Automotive.java
+++ src/model/Automotive.java
@@ -1,27 +1,59 @@
 package model;
 
 public class Automotive {
-	String name;
-	OptionSet opset[];
+	protected String name;
+	protected float basePrice;
+	protected OptionSet opset[];
 	
-	Automotive(int size, String name){
-		opset = new OptionSet[size];
-		this.name = name;
+	//Default constructor.
+	Automotive(){
+		this.opset = null;
+		this.name = null;
+		this.basePrice = 0;
 	}
 	
-	class OptionSet{
-		Option opt[];
-		String name;
+	//Constructor that sets up data.
+	Automotive(int size, String name, float price){
+		this.opset = new OptionSet[size];
+		this.name = name;
+		this.basePrice = 0;
+		
+		//To avoid nullPtr errors we use this for loop.
+		for (int i = 0; i < opset.length; i++) {
+			opset[i] = new OptionSet();
+		}
+	}
+	
+	//Inner class for the OptionSet contained in Automotive.
+	protected class OptionSet{
+		protected Option opt[];
+		protected String name;
+		
+		OptionSet(){
+			opt = null;
+			name = null;
+		}
 		
 		OptionSet(String setName, int setSize){
 			opt = new Option[setSize];
 			name = setName;
 			
+			for(int i = 0; i < opt.length; i++) {
+				opt[i] = new Option();
+			}
 		}
 		
-		class Option{
-			String name;
-			float price;
+		//Inner class for the option data type contained in OptionSet.
+		protected class Option{
+			protected String name;
+			protected String description;
+			protected float price;
+			
+			Option(){
+				name = null;
+				description = null;
+				price = 0;
+			}
 		}	
 	}
 }
